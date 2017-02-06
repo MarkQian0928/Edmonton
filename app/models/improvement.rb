48e5628eebc36ff16dfc70cf3541a7831a0e309d
Mark Qian <markqian199413@gmail.com>
2017-02-05 17:33:58 -0700

    like button succeed

--- app/models/improvement.rb
+++ app/models/improvement.rb
@@ -2,5 +2,5 @@ class Improvement < ApplicationRecord
 	validates :content, presence: true
 	validates :name, presence: true
 	validates :email, presence:true
-	acts_as_votable
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,8 +2,9 @@ Rails.application.routes.draw do
   mount LikeDislike::Engine, at: '/'
   resources :likes
   resources :improvements
+  resources :contents
+  resources :users
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-  root 'likes#vote'
-  get '/vote', to: 'likes/vote'
+  root 'likes#new'
 
 end
